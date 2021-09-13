b87c2ac13ae4bd40b34b53507192f7f7de855d13
airtonm4 <airton.atmi@gmail.com>
2021-09-13 20:05:10 -0300

    controllers created

--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,8 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-
-import { SettingsRepository } from "./repositories/settingsRepository";
+import { SettingsControllers } from "./controllers/SettingsController";
 
 const routes = Router()
-
-routes.post("/settings", async (request, response) => {
-    const {chat, username} = request.body;
-
-    const settingsRepository = getCustomRepository(SettingsRepository)
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    })
-    
-    await settingsRepository.save(settings)
-
-    return response.json(settings)
-})
+const settingsController = new SettingsControllers()
+routes.post("/settings", settingsController.create)
 
 export { routes }
